c0ebb194aae2b5224768ce18a731a85db0469047
wenjlo <wenjlo77@gmail.com>
2025-02-28 16:52:38 +0800

    fix code

--- add.py
+++ add.py
@@ -1,5 +1,7 @@
 from config import usernames
 import pandas as pd
+#新增帳號的程式
+
 df = pd.DataFrame(columns=['檔名','文案','tag'])
 for user in usernames:
     df.to_csv( f"C:/data/{user}.csv",index=False)
